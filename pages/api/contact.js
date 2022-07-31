da5e9ddeb5a17d5101f09c248637a99fc7a56fa4
Augustorresfx <augustorresfx@gmail.com>
2022-07-31 19:38:15 -0300

    contact-nodemailer-async

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,32 +1,53 @@
-export default function (req, res) {
-    require('dotenv').config()
-  const PASSWORD = process.env.password;
-    let nodemailer = require('nodemailer')
-    const transporter = nodemailer.createTransport({
-      port: 465,     
-      host: "smtp.gmail.com",
-         auth: {
-              user: 'agustorres633@gmail.com',
-              pass: PASSWORD,
-           },
-      secure: true,
+const nodemailer = require("nodemailer");
+
+export default async (req, res) => {
+
+const transporter = nodemailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+        user: "agustorres633@gmail.com",
+        pass: process.env.password,
+    },
+    secure: true,
+});
+
+await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transporter.verify(function (error, success) {
+        if (error) {
+            console.log(error);
+            reject(error);
+        } else {
+            console.log("Server is ready to take our messages");
+            resolve(success);
+        }
     });
-    
-    const mailData = {
-        from: 'agustorres633@gmail.com',
-        to: 'augustorresfx@gmail.com',
-        subject: `Message From ${req.body.name}`,
-        text: req.body.message + " | Sent from: " + req.body.email,
-        html: `<div>${req.body.message}</div><p>Sent from: ${req.body.email}</p>`
-    }
-  
-    transporter.sendMail(mailData, function (err, info) {
-        if(err)
-          console.log(err)
-        else
-          console.log(info);
-    })
-  
-    console.log(req.body)
-    res.send('success')
-  }
+});
+
+const mailData = {
+    from: {
+        name: req.body.name,
+        address: "agustorres633@gmail.com",
+    },
+    to: "augustorresfx@gmail.com",
+    subject: `form message`,
+    text: req.body.message + " | Sent from: " + req.body.email,
+    html: `<div>${req.body.message}</div><p>Sent from: ${req.body.email}</p>`,
+};
+
+await new Promise((resolve, reject) => {
+    // send mail
+    transporter.sendMail(mailData, (err, info) => {
+        if (err) {
+            console.error(err);
+            reject(err);
+        } else {
+            console.log(info);
+            resolve(info);
+        }
+    });
+});
+
+res.status(200).json({ status: "OK" });
+};
